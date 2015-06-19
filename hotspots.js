49253ff7cfb3aba8cdf239f75d98cf9365f61321
jessicatsengj <jessica834945@yahoo.com.tw>
2015-06-19 10:24:51 +0800

    21

--- hotspots.js
+++ hotspots.js
@@ -38,4 +38,31 @@ $(function(){
             $('#vlad').stop(true).animate({ opacity: 0}, duration);
         });
 
+
+        $("#conv").mouseover(function(){
+        $("#conv").stop(true).animate({
+            opacity:'0.5'});  
+        });
+        $("#conv").mouseout(function(){
+        $("#conv").stop(true).animate({
+            opacity:'1'});  
+        });
+        $("#alph").mouseover(function(){
+            $("#alph").stop(true).animate({
+            opacity:'0.5'});  
+        });
+        $("#alph").mouseout(function(){
+        $("#alph").stop(true).animate({
+            opacity:'1'});  
+        });
+        $("#abou").mouseover(function(){
+        $("#abou").stop(true).animate({
+            opacity:'0.5'});  
+        });
+        $("#abou").mouseout(function(){
+        $("#abou").stop(true).animate({
+            opacity:'1'});  
+        });
+
+
 });
